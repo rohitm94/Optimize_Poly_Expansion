bea163b793a39725bfca672e29794a81a1243fed
rohitm94 <rohit11094@gmail.com>
2019-11-03 15:42:25 -0500

    mallochost on host memory

--- p_lock_lat.cu
+++ p_lock_lat.cu
@@ -14,7 +14,8 @@ int main(int argc, char *argv[])
     int degree = atoi(argv[2]);
     int nbiter = atoi(argv[3]);
 
-    float *array = new float[n];
+    float *array ;
+    cudaMallocHost((void **)&array, n * sizeof(float));
     float *poly = new float[degree + 1];
     for (int i = 0; i < n; ++i)
         array[i] = 1.;
@@ -24,7 +25,7 @@ int main(int argc, char *argv[])
 
     float *d_array;
 
-    cudaMallocHost((void **)&d_array, n * sizeof(float));
+    cudaMalloc((void **)&d_array, n * sizeof(float));
 
     std::chrono::time_point<std::chrono::system_clock> begin, end;
     begin = std::chrono::system_clock::now();
@@ -35,7 +36,8 @@ int main(int argc, char *argv[])
     end = std::chrono::system_clock::now();
     std::chrono::duration<double> totaltime = (end - begin);
 
-    cudaFreeHost(d_array);
+    cudaFree(d_array);
+    cudaFreeHost(array);
 
     std::cout << "Latency of "<< nbiter <<"times is " << totaltime.count() << std::endl;
     std::cout << "Latency of PCI express is " << totaltime.count() / nbiter << std::endl;
