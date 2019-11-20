2bbfc7246c0117ec4e0fc193b1747e915d1152c7
rohitm94 <rohit11094@gmail.com>
2019-11-20 12:35:57 -0500

    part 2 of aas6

--- pipelined_poly/gpu_lock_stream.sh
+++ pipelined_poly/gpu_lock_stream.sh
@@ -1,6 +1,13 @@
-rm -rf gpu_bandwidth.txt
+rm -rf multistream_bandwidth.txt
 
-for n in `seq 250000000 250000000 1000000000`
-	do
-		./gpu_lock_bw $n 0 >> gpu_bandwidth.txt
-	done
+for n in  `seq 100 100 999` \
+		  `seq 1000 1000 9999` \
+		  `seq 10000 10000 99999` \
+		  `seq 100000 100000 999999` \
+		  `seq 1000000 1000000 9999999` \
+		  `seq 10000000 10000000 99999999` \
+		  `seq 100000000 100000000 999999999` \
+		  `seq 1000000000 1000000000 10000000000`
+        do
+                ./gpu_multi_stream $n 0 >> multistream_bandwidth.txt
+        done
